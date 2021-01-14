edfa1c16714cf12f765399d480abc141d2866f95
26ANSH <54389184+26ANSH@users.noreply.github.com>
2021-01-14 22:07:35 +0530

    Create Template.cpp

---
+++ Template.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int test;
+    cin>>test;
+    
+    while(test--)
+    {
+        
+    }
+    
+	return 0;
+}

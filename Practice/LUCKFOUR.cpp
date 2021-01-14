ab3e9e23ba2b5595da21a3b4e1054824dc6dc738
26ANSH <54389184+26ANSH@users.noreply.github.com>
2021-01-14 22:26:12 +0530

    Update LUCKFOUR.cpp

--- Practice/LUCKFOUR.cpp
+++ Practice/LUCKFOUR.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+#include <string>
+using namespace std;
 
+int main() 
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int test;
+    cin>>test;
+    
+    while(test--)
+    {
+        int count =0;
+        string num;
+        cin>>num;
+        
+        for(char digit : num)
+            if(digit == '4')
+                count++;
+        
+        cout<<count<<endl;
+    }
+    
+	return 0;
+}

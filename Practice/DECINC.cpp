d75c6b11e1cbac6bdf994aed08ba8e96e21621ff
26ANSH <54389184+26ANSH@users.noreply.github.com>
2021-01-15 09:16:56 +0530

    Create DECINC.cpp

---
+++ Practice/DECINC.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int num;
+    cin>>num;
+    
+    if(num%4 ==0)
+    cout<<num+1<<endl;
+    else
+    cout<<num-1<<endl;
+	return 0;
+}

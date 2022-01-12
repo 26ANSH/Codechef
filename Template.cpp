26052385fded0b69b53722d9a34c216a65174528
26ANSH <54389184+26ANSH@users.noreply.github.com>
2022-01-12 20:12:52 +0530

    updated template with macros

--- Template.cpp
+++ Template.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 using namespace std;
 
+#define fastIO ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define testloop int test;cin>>test;while(test--)
+
+
 int main() 
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    
-    int test;
-    cin>>test;
-    
-    while(test--)
+    fastio	
+	
+    testloop
     {
         
     }

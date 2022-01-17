b58e7acf4a98d15e62c62a59583f88a9e0044f81
26ANSH <54389184+26ANSH@users.noreply.github.com>
2022-01-17 19:18:19 +0530

    Update Template.cpp

--- Template.cpp
+++ Template.cpp
@@ -3,7 +3,9 @@ using namespace std;
 
 #define fastIO ios_base::sync_with_stdio(false);cin.tie(NULL);
 #define testloop int test;cin>>test;while(test--)
-
+#define ll long long int
+#define ull unsigned long long int
+#define loop(a, b) for(int i = a; i<b; i++)
 
 int main() 
 {
